87279e964d5b74ada1c9980aeddca4b115321132
FeProg-18 <79095518+felipe-programador-18@users.noreply.github.com>
2021-09-15 20:14:13 -0300

    c1- subindo para ver os erros

--- pages/api/get-promo.js
+++ pages/api/get-promo.js
@@ -1,19 +1,20 @@
 import  { GoogleSpreadsheet } from 'google-spreadsheet'
 
 
-const doc = new GoogleSpreadsheet(process.env.SHEET_DOC_ID)
+const doc = new GoogleSpreadsheet(process.env.SHEET_DOC_ID) 
+
+const fromBase64 = () => {
+    const buff = Buffer.from(value,'base64');
+    return buff.toString('ascii');
+
+}
 
 
-
-export default async ( req, res) => {
-     console.log(process.env.VAR1)
-
-
+export default async ( req, res) => {    
     try{
-       
-        await doc.useServiceAccountAuth({
-            client_email: process.env.SHEET_CLIENT_EMAIL ,
-            private_key: process.env.SHEET_PRIVATE_KEY
+       await doc.useServiceAccountAuth({
+        client_email: process.env.SHEET_CLIENT_EMAIL ,
+        private_key: fromBase64(process.env.SHEET_PRIVATE_KEY)
         
         })
         await doc.loadInfo()
--- pages/api/save.js
+++ pages/api/save.js
@@ -1,6 +1,7 @@
 import  { GoogleSpreadsheet } from 'google-spreadsheet'
 import moment from 'moment'
 
+
 const doc = new GoogleSpreadsheet(process.env.SHEET_DOC_ID)
 
  const genCoupon = ()=>{
@@ -14,8 +15,7 @@ export default async ( req, res) => {
     try{
       await doc.useServiceAccountAuth({
         client_email: process.env.SHEET_CLIENT_EMAIL ,
-        private_key: process.env.SHEET_PRIVATE_KEY
-    
+        private_key:process.env.SHEET_PRIVATE_KEY
     })
         await doc.loadInfo()
         //Nome	Email	Whatsaap	Cupon	Promo
